22ff094d46f628e5dd495912dc1cb72d071ba95d
Neo <watcharawut.mas@gmail.com>
2024-07-07 08:15:53 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,16 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, addr = server_socket.accept()
     print(addr)
-    response = f"HTTP/1.1 200 OK\r\n\r\nyour address is {addr}\r\n"
+    data = client.recv(2048).decode()
+    decoded = data.split("\r\n")
+    target, header, body = decoded[0], decoded[1:-2], decoded[-1] 
+    response_status = "200 OK"
+    response_body = f"your address is {addr}"
+    if target.split(" ")[1] != "/":
+        response_status = "404 Not Found"
+
+
+    response = f"HTTP/1.1 {response_status}\r\n\r\n{response_body}"
     client.send(response.encode())
 
 if __name__ == "__main__":
